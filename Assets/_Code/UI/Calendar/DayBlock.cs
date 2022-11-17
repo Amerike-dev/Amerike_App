97fa3298b7ef7dd13287611bf2b6ea93437dd05a
Dino <diana_ramos_g@hotmail.com>
2022-11-17 13:44:22 -0600

    estructura botones

--- Assets/_Code/UI/Calendar/DayBlock.cs
+++ Assets/_Code/UI/Calendar/DayBlock.cs
@@ -8,6 +8,7 @@ public class DayBlock : MonoBehaviour
 
     [SerializeField] private Text dayWeekTxt;
     [SerializeField] private Text dayNumberTxt;
+    [SerializeField] private List<HourBlock>hourButtons;
 
 
     public void SetData(string dayWeek, string dayNumber)
--- Assets/_Code/UI/Calendar/HourBlock.cs
+++ Assets/_Code/UI/Calendar/HourBlock.cs
@@ -4,21 +4,26 @@ using UnityEngine;
 
 public class HourBlock : MonoBehaviour
 {
-  
-  
+    [SerializeField] private HourSelected myHour;
   
   
   public void SelectHour()
   {
     
   }
+  
+  
 }
 
 public enum HourSelected
 {
-     none,
-     
-     
-     
+     None,
+     SevenToNine_AM,
+     NineToEleven_AM,
+     ElevenToOne_AM,
+     OneToThree_PM,
+     ThreeToFive_PM,
+     FiveToSeven_PM,
+     SevenToNine_PM
      
 }
